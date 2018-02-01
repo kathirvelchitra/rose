863ce63c92a209d265b00c149a6ace9c6bbec467
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-02-01 08:54:32 +0000

    Create range.c

---
+++ range.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n=='1'||n=='2'||n=='3'||n=='4'||n=='5'||n=='6'||n=='7'||n=='8'||n=='9')
+    {
+        printf("yes");
+   }
+   else
+   { 
+        printf("no");
+    }
+}
