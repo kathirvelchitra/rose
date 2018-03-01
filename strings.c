696aa8ce4f3a648cc86f66f925fceb3fc1904100
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-03-01 15:55:43 +0530

    Create strings.c

---
+++ strings.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[20];
+    int i;
+    printf("enter the string:");
+    scanf("%s",&a);
+    for(i=0;i<=a[i];i++)
+    {
+        if(a[i]>='0'&&a[i]<='9')
+        printf("\n %c",a[i]);
+    }
+}
